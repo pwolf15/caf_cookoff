6aad4211fabd4590edd1e881b7df324873ee7663
pwolf15 <patrick.wolf@live.com>
2022-05-07 15:55:01 -0700

    Start adding cross-correlation

--- caf_cpp/caf.cpp
+++ caf_cpp/caf.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
 #include <string>
 #include <complex>
+#include <complex.h>
 #include <fstream>
 #include <vector>
 #include <cstring>
+#include <math.h>
+#include "fftw3.h"
+
+void xcorr(std::vector<std::complex<float>>& signala, 
+  std::vector<std::complex<float>> signalb, 
+  std::vector<std::complex<float>> result, int N)
+{
+    std::vector<std::complex<float>> signala_ext(2*N-1), signalb_ext(2*N-1), out_shifted(2*N-1);
+    std::vector<std::complex<float>> outa(2*N-1), outb(2*N-1), out(2*N-1);
+
+    fftw_plan pa = fftw_plan_dft_1d(2 * N - 1, reinterpret_cast<fftw_complex*>(signala_ext.data()), reinterpret_cast<fftw_complex*>(outa.data()), FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_plan pb = fftw_plan_dft_1d(2 * N - 1, reinterpret_cast<fftw_complex*>(signalb_ext.data()), reinterpret_cast<fftw_complex*>(outb.data()), FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_plan px = fftw_plan_dft_1d(2 * N - 1, reinterpret_cast<fftw_complex*>(out.data()), reinterpret_cast<fftw_complex*>(result.data()), FFTW_BACKWARD, FFTW_ESTIMATE);
+
+    //zeropadding
+    for (int i = N - 1; i <= N + (N - 1); ++i)
+    {
+      signala_ext[i] = signala[i - (N - 1)];
+    }
+    for (int i = N; i <= N + (N - 1); ++i)
+    {
+      signalb_ext[i] = signalb[i - N];
+    }
+
+    fftw_execute(pa);
+    fftw_execute(pb);
+
+    std::complex<float> scale = 1.0/(2 * N -1);
+    for (int i = 0; i < 2 * N - 1; i++)
+        out[i] = outa[i] * conj(outb[i]) * scale;
+
+    fftw_execute(px);
+
+    fftw_destroy_plan(pa);
+    fftw_destroy_plan(pb);
+    fftw_destroy_plan(px);
+
+    fftw_cleanup();
+
+    return;
+}
 
 std::vector<std::complex<float>> read_complex(std::string filename)
 {
@@ -19,11 +61,11 @@ std::vector<std::complex<float>> read_complex(std::string filename)
   return data;
 };
 
-std::vector<std::complex<float>> apply_fdoa(std::vector<std::complex<float>> ray, float fdoa, float sample_rate)
+std::vector<std::complex<float>> apply_fdoa(std::vector<std::complex<float>>& ray, float fdoa, float sample_rate)
 {
-  std::complex<float> precache(0,2*3.14159265*fdoa/sample_rate);
+  std::complex<float> precache(0,2*3.141592653589*fdoa/sample_rate);
   std::vector<std::complex<float>> new_ray(ray.size());
-  for (size_t i = 0; i < new_ray.size(); ++i)
+  for (size_t i = 0; i < 10; ++i)
   {
     new_ray[i] = ray[i] * std::exp(precache*std::complex<float>(i,i));
   }
@@ -31,7 +73,22 @@ std::vector<std::complex<float>> apply_fdoa(std::vector<std::complex<float>> ray
   return new_ray;
 }
 
-std::vector<float> amb_surf(
+std::vector<float> xcor(std::vector<std::complex<float>>& apple, std::vector<std::complex<float>>& banana)
+{
+  std::vector<std::complex<float>> corr(2 * apple.size() - 1);
+
+  xcorr(apple, banana, corr, apple.size());
+
+  std::vector<float> amplitudes;
+  for (int i = 0; i < corr.size(); ++i)
+  {
+    amplitudes.push_back(sqrt(pow(corr[i].real(), 2) + pow(corr[i].imag(), 2)));
+  }
+
+  return amplitudes;
+}
+
+std::vector<std::vector<float>> amb_surf(
   std::vector<std::complex<float>>& needle,
   std::vector<std::complex<float>>& haystack, 
   std::vector<float>& freqs_hz,
@@ -40,10 +97,12 @@ std::vector<float> amb_surf(
   uint32_t len_needle = needle.size();
   uint32_t len_haystack = haystack.size();
   uint32_t len_freqs = freqs_hz.size();
-  std::vector<float> surf(len_freqs*len_needle);
-  for (size_t i = 0; i < len_freqs; ++i)
+  std::vector<std::vector<float>> surf(len_freqs);
+  for (size_t i = 0; i < 10; ++i)
   {
     std::vector<std::complex<float>> shifted = apply_fdoa(needle, freqs_hz[i], sample_rate);
+    std::cout << shifted[i] << std::endl;
+    surf[i] = xcor(shifted, haystack);
   }
   
 
